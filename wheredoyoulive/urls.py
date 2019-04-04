680e0513ddd8b2668bdb0b574c54e0481856d76a
jeremyzhou <jeremy.q.zhou@gmail.com>
2019-04-04 13:49:48 -0400

    added more to urls + views (jaswin)

--- wheredoyoulive/urls.py
+++ wheredoyoulive/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = "wheredoyoulive"  # idk if I need this
 urlpatterns = [
     path('', views.index),
-    path('make_user/<str:username>', views.make),
-    path('upd_user/<str:username>', views.update),
+    path('make_user', views.make),
+    path('make_user/<str:dname>/<str:uname>/<str:lat>/<str:long>', views.make_info),
+    path('upd_user', views.update),
+    path('upd_user/<str:dname>/<str:uname>/<str:lat>/<str:long>', views.update_info),
     path('del_user/<str:username>', views.delete),
 ]
--- wheredoyoulive/views.py
+++ wheredoyoulive/views.py
@@ -1,20 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('Homepage')
 
-# Make new user
-def make(request, username):
-    u = User(username=username, latitude=0, longitude=0)
-    u.save()
-    return HttpResponse('Made %s' % username)
 
-def update(request, username):
-    return HttpResponse('Updated %s' % username)
+def make(request):
+    # get info from form
+    return HttpResponseRedirect()
+
+def make_info(request, dname, uname, lat, long):
+    u = User(name=dname, username=uname, latitude=lat, longitude=long)
+    u.save()
+    # return HttpResponse('Made %s' % username)
+    return HttpResponseRedirect('')
+
+def update(request):
+    # return HttpResponse('Updated %s' % username)
+
+def update_info(request, dname, uname, lat, long):
+    u = User.objects.get(username=uname)
+    u.update(name=dname, latitude=lat, longitude=long)
 
 def delete(request, username):
     u = User.objects.get(username=username)
     u.delete()
-    return HttpResponse('Deleted %s' % username)
+    # return HttpResponse('Deleted %s' % username)
+    return HttpResponseRedirect('')
