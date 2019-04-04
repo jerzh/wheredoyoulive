d6eb2a94ea16df35490e02a5cc81c5d44b3254eb
jeremyzhou <jeremy.q.zhou@gmail.com>
2019-04-04 12:49:28 -0400

    urls + views small edits

--- wheredoyoulive/urls.py
+++ wheredoyoulive/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "wheredoyoulive"  # idk if I need this
 urlpatterns = [
     path('', views.index),
-    path('make_user/<str:id>', views.make),
-    path('upd_user/<str:id>', views.update),
-    path('del_user/<str:id>', views.delete),
+    path('make_user/<str:username>', views.make),
+    path('upd_user/<str:username>', views.update),
+    path('del_user/<str:username>', views.delete),
 ]
--- wheredoyoulive/views.py
+++ wheredoyoulive/views.py
@@ -6,11 +6,15 @@ def index(request):
     return HttpResponse('Homepage')
 
 # Make new user
-def make(request, id):
-    return HttpResponse('Made %s' % id)
+def make(request, username):
+    u = User(username=username, latitude=0, longitude=0)
+    u.save()
+    return HttpResponse('Made %s' % username)
 
-def update(request, id):
-    return HttpResponse('Updated %s' % id)
+def update(request, username):
+    return HttpResponse('Updated %s' % username)
 
-def delete(request, id):
-    return HttpResponse('Im afraid you cant do that %s' % id)
+def delete(request, username):
+    u = User.objects.get(username=username)
+    u.delete()
+    return HttpResponse('Deleted %s' % username)
